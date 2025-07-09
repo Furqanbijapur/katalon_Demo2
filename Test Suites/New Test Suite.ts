f579802b21b15f77fbec3c45737005258ac2c62b
Furqan <furqana043@gmail.com>
2025-07-10 02:56:00 +0530

    second

--- Test Suites/New Test Suite.ts
+++ Test Suites/New Test Suite.ts
@@ -11,4 +11,18 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>928247fb-a050-4e70-bae0-227fc628079b</testSuiteGuid>
+   <testCaseLink>
+      <guid>e0a0f4fd-6e5c-4b70-b29c-f1e7c1bb39ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>74c9d2ef-a2f1-477f-b389-535eddec9fb0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
